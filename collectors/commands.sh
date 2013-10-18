e445a203cc2847106540267d4a5b6e23b0b92f66
Joseph Adams <whitegolem@gmail.com>
2013-10-18 18:38:09 +0200

    updated sh command to work from root dir of qs

--- collectors/commands.sh
+++ collectors/commands.sh
@@ -1,3 +1,5 @@
-echo "start_ts,end_ts,duration,program" > activity_log.csv; for f in ~/data/misc/activity_log/2013*.log; do sed -n "1~3p" $f | python activity.py >> activity_log.csv; done
+echo "start_ts,end_ts,duration,program" > data/activity_log.csv; for f in ~/data/misc/activity_log/2013*.log; do sed -n "1~3p" $f | python collectors/activity.py >> data/activity_log.csv; done
 
 sed -n "1~3p" ~/data/misc/activity_log/2013*.log
+
+echo "date,mib" > data/net_usage_daily.csv; tail -qn 1 ~/data/misc/activity_log/net_2013* | cut -d, -f1,3 >> data/net_usage_daily.csv
