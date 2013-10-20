ae81518a2c8e950c9e97e1787bf1475a1579a117
Joseph Adams <whitegolem@gmail.com>
2013-10-20 12:52:27 +0200

    added sh command to get open tab distribution

--- collectors/commands.sh
+++ collectors/commands.sh
@@ -3,3 +3,5 @@ echo "start_ts,end_ts,duration,program" > data/activity_log.csv; for f in ~/data
 sed -n "1~3p" ~/data/misc/activity_log/2013*.log
 
 echo "date,mib" > data/net_usage_daily.csv; tail -qn 1 ~/data/misc/activity_log/net_2013* | cut -d, -f1,3 >> data/net_usage_daily.csv
+
+echo "count,n" > data/tab_distr.csv; cat ~/data/misc/activity_log/2013* | sed -n "1~3p" | cut -d, -f7 | sed /^$/d | sort | uniq -c | sort | sed "s/^ *//g" | tr " " "," >> data/tab_distr.csv
