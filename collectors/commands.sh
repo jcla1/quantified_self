f62bc61820a81ef42582b3fea64d71dec971808f
Joseph Adams <whitegolem@gmail.com>
2013-11-01 18:25:38 +0100

    added commands to gather search/tab/sites distributions

--- collectors/commands.sh
+++ collectors/commands.sh
@@ -1,7 +1,9 @@
 echo "start_ts,end_ts,duration,program" > data/activity_log.csv; for f in ~/data/misc/activity_log/2013*.log; do sed -n "1~3p" $f | python collectors/activity.py >> data/activity_log.csv; done
 
-sed -n "1~3p" ~/data/misc/activity_log/2013*.log
-
 echo "date,mib" > data/net_usage_daily.csv; tail -qn 1 ~/data/misc/activity_log/net_2013* | cut -d, -f1,3 >> data/net_usage_daily.csv
 
 echo "count,n" > data/tab_distr.csv; cat ~/data/misc/activity_log/2013* | sed -n "1~3p" | cut -d, -f7 | sed /^$/d | sort | uniq -c | sort | sed "s/^ *//g" | tr " " "," >> data/tab_distr.csv
+
+echo "date" > data/search_distr.csv; cat ~/data/misc/activity_log/2013* | sed 'N;N;s/^\(.*\)\n.*\n\(.*\)$/\1,\2/' | cut -d, -f1,8 | grep www.google.com/search | cut -d, -f1 >> data/search_distr.csv;
+
+#echo "date" > data/sites_distr.csv; cat ~/data/misc/activity_log/2013* | sed 'N;N;s/^\(.*\)\n.*\n\(.*\)$/\1,\2/' | cut -d, -f1,8 | tr "," " " | uniq -f1 | cut -d" " -f1 >> data/sites_distr.csv
