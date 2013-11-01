113878324f4e90d1c011057fe14d4014113eab6a
Joseph Adams <whitegolem@gmail.com>
2013-11-01 18:32:49 +0100

    Added search_distr code

---
+++ board/search_distr.js
@@ -0,0 +1,53 @@
+d3.csv("/data/search_distr.csv", function(err, raw) {
+  var margin = {top: 20, right: 20, bottom: 40, left: 20},
+    width = 360 - margin.left - margin.right,
+    height = 170 - margin.top - margin.bottom;
+
+  var formatTime = formatTime = d3.time.format("%H:%M"),
+    formatHours = function(d) { return formatTime(new Date(2013, 0, 1, d, 0)) };
+
+  var values = raw.map(function(a) {
+    return (new Date(parseInt(a.date)*1000)).getHours();
+  });
+
+  var x = d3.scale.linear()
+    .domain([0, 23])
+    .range([0, width]);
+
+  var data = d3.layout.histogram()
+    .bins(x.ticks(20))
+    (values);
+
+  var xAxis = d3.svg.axis()
+    .scale(x)
+    .ticks(4)
+    .orient("bottom")
+    .tickFormat(formatHours);
+
+  var y = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d.y; })])
+    .range([height, 0]);
+
+  var svg = d3.select("#search_distr").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var bar = svg.selectAll(".bar")
+    .data(data)
+  .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+  bar.append("rect")
+    .attr("x", 0)
+    .attr("width", x(data[0].dx) - 1)
+    .attr("height", function(d) { return height - y(d.y); });
+
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+
+});
