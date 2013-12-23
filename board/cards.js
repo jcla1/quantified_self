01306a6433c2719613ec1a5059414c04b8ef1d39
Joseph Adams <whitegolem@gmail.com>
2013-12-23 18:22:30 +0100

    removed useless log statement

--- board/cards.js
+++ board/cards.js
@@ -15,7 +15,6 @@ d3.csv("/data/stats.csv", function(err, data) {
     .html(function(d) { return d.subtext; });
 
   cards.each(function(d) {
-    console.log(d)
     if (!Number(d.special)) return;
     this.style.boxShadow = "0 0 12px " + (d.color ? d.color : "rgba(255, 215, 0, 0.8)");
   });
