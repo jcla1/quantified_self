0dac9e12dd87205d9c91063b5836801c3f012724
Joseph Adams <whitegolem@gmail.com>
2014-01-02 16:33:48 +0100

    Finally finished for publishing

--- collectors/commands.sh
+++ collectors/commands.sh
@@ -1,5 +1,6 @@
 LOGS=~/data/misc/activity_log/2013*.log
 NET_LOGS=~/data/misc/activity_log/net_2013*
+NUM_DAYS=110
 
 echo "start_ts,end_ts,duration,program" > data/activity_log.csv;
 echo "timestart,occ" > data/computer_probability.csv;
@@ -11,6 +12,8 @@ echo "count,n" > data/tab_distr.csv; cat $LOGS | sed -n "1~3p" | cut -d, -f7 | s
 
 echo "date" > data/search_distr.csv; cat $LOGS | sed 'N;N;s/^\(.*\)\n.*\n\(.*\)$/\1,\2/' | cut -d, -f1,8 | grep www.google.com/search | cut -d, -f1 >> data/search_distr.csv;
 
-echo "title,value,subtext,special,color" > data/stats.csv; sed -n "3~3p" $LOGS | python collectors/url_stats.py >> data/stats.csv;
+echo "title,value,subtext,special,color" > data/stats.csv;
+sed -n "3~3p" $LOGS | python collectors/url_stats.py >> data/stats.csv;
+tail -n +2 data/net_usage_daily.csv | awk -F"," -v "NUM_DAYS=$NUM_DAYS" -v OFS="," '{ sum+=$2 } END {u=sum/1024; printf("network usage,%dGiB,+ <span>%dMiB</span> / day,,\n", u, (u*1024)/NUM_DAYS)}' >> data/stats.csv
 
 # echo "date" > data/sites_distr.csv; cat $LOGS | sed 'N;N;s/^\(.*\)\n.*\n\(.*\)$/\1,\2/' | cut -d, -f1,8 | tr "," " " | uniq -f1 | cut -d" " -f1 >> data/sites_distr.csv
