98d90cdb35a557b5bd7cb8dbe96cc937eb19a1f7
Joseph Adams <whitegolem@gmail.com>
2014-06-30 15:14:35 +0200

    Added Battery capacity in tracking

--- cron_scripts/activity.sh
+++ cron_scripts/activity.sh
@@ -12,13 +12,16 @@ function log_activity()
 {
     local UTC=`date "+%s,%Z"`
     local IDLE=$((`/usr/sbin/ioreg -c IOHIDSystem | sed -e '/HIDIdleTime/!{ d' -e 't' -e '}' -e 's/.* = //g' -e 'q'` / 1000000000 ))
+
+    local BATTERY_CHARGE=$(/usr/sbin/ioreg -n AppleSmartBattery -r | awk '$1~/Capacity/{c[$1]=$3} END{OFMT="%.1f"; max=c["\"MaxCapacity\""]; print (max>0? 100*c["\"CurrentCapacity\""]/max: "?")}')
+
     local ACTIVE_PROGRAM=$(/usr/bin/osascript $FRONT_APP)
 
     read CHROME_STATS ACTIVE_URL <<< `/usr/bin/osascript $CHROME_STATUS`
 
     local PROGRAMS=$(/usr/bin/osascript $RUNNING_PROGRAMS)
 
-    echo "$UTC,$IDLE,$ACTIVE_PROGRAM,$CHROME_STATS\n$PROGRAMS\n$ACTIVE_URL" >> $LOG_FILE
+    echo "$UTC,$IDLE,$ACTIVE_PROGRAM,$CHROME_STATS,$BATTERY_CHARGE\n$PROGRAMS\n$ACTIVE_URL" >> $LOG_FILE
 
     #echo "`vnstat --dumpdb | grep "^d;0" | tr ";" "," | cut -d, -f 3-`" >> $LOG_FILE
 }
